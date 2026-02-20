5128e0be7bd5aa7239e0a9f3d1e7b7792ecf7be1
ItsAnushkarajvanshi <anushkarajvanshi7@gmail.com>
2026-02-20 16:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 19 MB (5.27%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+     vector<vector<int>>p=matrix;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(p[i][j]==0){
+                    for(int k=0;k<m;k++){
+                        matrix[i][k]=0;
+                    }
+                    for(int k=0;k<n;k++){
+                        matrix[k][j]=0;
+                    }
+                }
+            }
+        }
+    }
+};
